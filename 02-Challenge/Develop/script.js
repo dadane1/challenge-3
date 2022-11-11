926bc2dbd6849a215aa46c98b3431e874ea6b5b3
Deborah Adane <debor@DESKTOP-8V9BF9H>
2022-11-10 23:15:19 -0600

    modified the javascript to allow password generation

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,6 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var lcchar = 'abcdefghijklmnopqrstuvwxyz';
+var ucchar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var numchar = '1234567890';
+var spechchar = '!@#$%^&*';
+var password = ''
+var characters = ''
+var choices = ''
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -9,6 +15,51 @@ function writePassword() {
   passwordText.value = password;
 
 }
+function generatePassword() {
+  var pw = ''
+  var pwlength = parseInt(window.prompt('How many characters do you want(8-128)'))
+  if (pwlength < 8 || pwlength > 128) {
+    window.alert('please choose between 8 & 128')
+    return
+  }
+  var lc = window.confirm('Do you want lowercase characters')
+  if (lc === true) {
+    characters+=lcchar
+  }
+  var uc = window.confirm('Do you want uppercase characters')
+  if (uc === true) {
+    characters+=ucchar
+  }
+  var num = window.confirm('Do you want numeric characters')
+  if (num === true) {
+    characters+=numchar
+  }
+  var spech = window.confirm('Do you want special characters')
+  if (spech === true) {
+    characters+=spechchar
+  } else {
+    window.alert('please choose one of these "!@#$%^&*"')
+    return
+  }
+  
+  console.log(characters)
+  if (lc + uc + num + spech < 1) {
+    window.alert('Please choose at least 1 character')
+    return
+  }
+  choices = characters.toString()
+  console.log(choices)
+  console.log("passwordlength",pwlength)
+  console.log(Math.floor(Math.random() * pwlength))
+  for (var i = 0; i < pwlength; i++) {
+    password += choices.charAt(Math.floor(Math.random() * choices.length));
+  }
+  return password
+}
 
-// Add event listener to generate button
+function getRandomIntInclusive(pwlength) {
+  console.log(password)
+  return password
+}
+
 generateBtn.addEventListener("click", writePassword);
